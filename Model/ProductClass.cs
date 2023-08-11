483be7679d8cd6cb8b9cff609688126d6181abde
Akash santhosh <akash777kk@gmail.com>
2023-08-11 16:56:38 +0530

    productApp

---
+++ Model/ProductClass.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp.Model
+{
+    internal class ProductClass
+    {
+        private int id;
+        private string name;
+        private int price;
+        private double discount=10.0;
+        private double discountAmount;
+        private double finalPrice;
+
+      
+        public ProductClass(int id, string name, int price) 
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+            CalculateDisscount(price, discount);
+        }
+        public ProductClass(int id, string name, int price, double discount):this(id,name,price)
+        {
+            
+            this.discount = discount;
+            CalculateDisscount(price, discount);
+
+        }
+
+        private void CalculateDisscount(int price, double discount)
+        {
+            
+            discountAmount = price * (discount / 100);
+            finalPrice = price - discountAmount;
+
+        }
+      /*  public string Calculate() {
+            
+            discountAmount = price * (discount / 100);
+            finalPrice = price - discountAmount;
+            return $"id : {id}   Name : {name}  Actual price :{price} Discount Amount : {finalPrice}";
+           
+        }*/
+
+        public string TotalPrice()
+        {
+            Console.WriteLine($"Disscount for product : {discount}% ");
+
+            return $"id : {id}   Name : {name}  Actual price :{price} Discount Amount : {finalPrice}";
+        }
+
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using ProductApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductClass productClass1 = new ProductClass(1,"akash",1000,50.0);
+            Console.WriteLine(productClass1.TotalPrice());
+           // Console.WriteLine(productClass1.Calculate());
+            ProductClass productClass2 = new ProductClass(2, "Akshay", 600);
+            Console.WriteLine(productClass2.TotalPrice());
+        }
+    }
+}
